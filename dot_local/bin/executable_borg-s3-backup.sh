464ff4ed12e237d8cc56fe3745e2b29ae69e3e5e
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2020-09-12 13:25:32 +0200

    feat(borg-s3-backup): keep monthly backups

--- dot_local/bin/executable_borg-s3-backup.sh
+++ dot_local/bin/executable_borg-s3-backup.sh
@@ -41,9 +41,9 @@ OPERATION_STATUS=$?
 
 # Only continue if backup was actually successful
 if [ $OPERATION_STATUS == 0 ]; then
-	# Clean up old backups: keep 7 end of day and 4 additional end of week archives.
+	# Clean up old backups
 	# Prune operation is not important, s3 sync is - do not exit were this to fail
-	borg prune -v --list --keep-daily=7 --keep-weekly=4
+	borg prune -v --list --keep-daily=7 --keep-weekly=4 -keep-monthly=12
 
 	# Sync borg repo to s3
 	printf "\n\n ** Sync to s3...\n"
