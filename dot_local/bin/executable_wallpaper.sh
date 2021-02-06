0fdcce485b3bac5806c047117e28975c8287ce97
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2021-02-06 13:36:08 +0100

    use imagemagick in wallpaper script instead of xwallpaper

--- dot_local/bin/executable_wallpaper.sh
+++ dot_local/bin/executable_wallpaper.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-ls ${XDG_DATA_HOME-$HOME/.local/share}/backgrounds/* 2> /dev/null | shuf -n 1 | xargs -r xwallpaper --zoom 
+res=$(xrandr | awk '/*/ {print $1}')
+file="$(ls ${XDG_DATA_HOME-$HOME/.local/share}/backgrounds/* 2> /dev/null | shuf -n 1)"
+[ -n "$file" ] && display -window root -resize $res\^ -gravity center -extent $res "$file"
