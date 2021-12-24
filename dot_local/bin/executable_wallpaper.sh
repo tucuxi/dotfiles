cc4faa6bf5e05466ee91f4ba4854efe90ab6c4d8
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2021-12-24 19:54:56 +0100

    feat: display and store random nature wallpaper from unsplash

--- dot_local/bin/executable_wallpaper.sh
+++ dot_local/bin/executable_wallpaper.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
-res=$(xrandr | awk '/*/ {print $1}')
-file="$(ls ${XDG_DATA_HOME-$HOME/.local/share}/backgrounds/* 2> /dev/null | shuf -n 1)"
-[ -n "$file" ] && display -window root -resize $res\^ -gravity center -extent $res "$file"
+resolution=$(xrandr | awk '/*/ {print $1}')
+
+nextfile=${XDG_DATA_HOME-$HOME/.local/share}/backgrounds/random-next.jpg
+currfile=${XDG_DATA_HOME-$HOME/.local/share}/backgrounds/random.jpg
+
+[ -f "$nextfile" ] && mv "$nextfile" "$currfile" 
+[ -f "$currfile" ] && display -window root -resize $resolution\^ -gravity center -extent $resolution "$currfile"
+
+sleep 1
+
+tempfile=$(mktemp)
+wget -q "https://source.unsplash.com/random/1920x1080?nature" -O "$tempfile" 2> /dev/null
+[ $? -eq 0 -a -s "$tempfile" ] && mv "$tempfile" "$nextfile"
