9a66d165dc2ea2e67e562b8be594e94e941bb597
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2022-01-01 12:17:12 +0100

    add support for retrieving wallpaper from bing

--- dot_local/bin/executable_wallpaper.sh
+++ dot_local/bin/executable_wallpaper.sh
@@ -1,14 +1,37 @@
-#!/bin/sh
-resolution=$(xrandr | awk '/*/ {print $1}')
+#!/bin/bash
 
-nextfile=${XDG_DATA_HOME-$HOME/.local/share}/backgrounds/random-next.jpg
-currfile=${XDG_DATA_HOME-$HOME/.local/share}/backgrounds/random.jpg
+# Requires: curl, imagemagick, jq
 
-[ -f "$nextfile" ] && mv "$nextfile" "$currfile" 
-[ -f "$currfile" ] && display -window root -resize $resolution\^ -gravity center -extent $resolution "$currfile"
+display_image () {
+	local resolution
+	resolution="$(xrandr | awk '/*/ {print $1}')"
+	display -window root -resize "$resolution^" -gravity center -extent "$resolution" "$1"
+}
 
-sleep 1
+load_image () {
+	curl --silent --fail --location "$image_url" --output "$1"
+}
 
-tempfile=$(mktemp)
-wget -q "https://source.unsplash.com/random/1920x1080?nature" -O "$tempfile" 2> /dev/null
-[ $? -eq 0 -a -s "$tempfile" ] && mv "$tempfile" "$nextfile"
+set_image_url_bing () {
+	local image_path
+	image_path="$(curl -s 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=4' | jq -r '.images[].url' | shuf -n 1)"
+	image_url="https://bing.com$image_path"
+}
+
+set_image_url_unsplash () {
+	image_url="https://source.unsplash.com/random/1920x1080?nature"
+}
+
+image_directory="${XDG_DATA_HOME-$HOME/.local/share}/backgrounds"
+next_image="${image_directory}/random-next.jpg"
+current_image="${image_directory}/random.jpg"
+
+[ -f "$next_image" ] && mv "$next_image" "$current_image" 
+[ -f "$current_image" ] && display_image "$current_image"
+
+sleep 2 # increase success rate if WiFi connection is still being established
+
+temp_file=$(mktemp)
+set_image_url_bing
+load_image "$temp_file"
+[ $? -eq 0 ] && [ -s "$temp_file" ] && mv "$temp_file" "$next_image"
